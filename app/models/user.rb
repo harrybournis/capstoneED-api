bc80511ce890d2f4770aa0e2a76d1fdb0d346c82
harry bournis <harrybournis@gmail.com>
2016-08-04 20:40:58 +0300

    added some user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,20 @@
 class User < ApplicationRecord
-  # Include default devise modules.
+
+  # authentication
+  #
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
 
   include DeviseTokenAuth::Concerns::User
-
   attr_accessor :confirm_success_url, :config_name
+
+  # validations
+  #
+  validates :first_name, :last_name, presence: true
 
   def full_name
   	"#{self.fist_name} #{self.last_name}"
   end
+
 end
