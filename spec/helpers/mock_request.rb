6540c4fb5c481e7ff0c9406b7b942a62981e3ef3
harry bournis <harrybournis@gmail.com>
2016-08-10 04:08:24 +0300

    started writing JWTAuthenticator module from scratch. Added MockRequest class for testing. Wrote basic request headers tests.

---
+++ spec/helpers/mock_request.rb
@@ -0,0 +1,67 @@
+class MockRequest
+
+	attr_reader :headers, :cookies, :body
+
+	def initialize(valid, args = nil)
+		valid ? valid_request_hardcoded(args) : invalid_request_hardcoded(args)
+	end
+
+
+private
+
+	def valid_request(headers=nil, cookies=nil, body=nil)
+		@headers = { "X-XSRF-TOKEN" => "NIBzka/3Plj8yg30+uYnyEBGunKPMhvG8ThF7EJxrBs=" }
+		@cookies = { "access-token" => "",
+					 "refresh-token" => "" }
+
+		@headers.merge!(headers) if headers
+		@cookies.merge!(cookies) if cookies
+		@body.merge!(body) 		 if body
+	end
+
+	def valid_request_hardcoded(headers=nil, cookies=nil, body=nil)
+		@headers = { "X-XSRF-TOKEN" => "NIBzka/3Plj8yg30+uYnyEBGunKPMhvG8ThF7EJxrBs=" }
+		@cookies = { "access-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjMwNDg2MjA1MzYsImp0aSI6Imk3c3FlRVNFREpIVVNCWmQ0SEpONDJvMSIsImlzcyI6ImxvY2FsaG9zdDozMDAwIiwiY3NyZl90b2tlbiI6Ik5JQnprYS8zUGxqOHlnMzArdVlueUVCR3VuS1BNaHZHOFRoRjdFSnhyQnM9In0.HHs3KKfsxkxdcSzeafU1FiXXMfeiomJehdfK9vlKTHQ",
+					 "refresh-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjMwODAxNTY4MTYsImp0aSI6Imk3c3FlRVNFREpIVVNCWmQ0SEpONDJvMSIsImlzcyI6ImxvY2FsaG9zdDozMDAwIn0.RFmIcEVA9-ANLgDSiCPfOYTnGFfnL-YcKVeJPqrE6L0" }
+
+		@headers.merge!(headers) if headers
+		@cookies.merge!(cookies) if cookies
+		@body.merge!(body) 		 if body
+	end
+
+	def invalid_request_hardcoded(headers=nil, cookies=nil, body=nil)
+		@headers = { "X-XSRF-TOKEN" => "NIBzka/3Plj8yg30+uYnyEBGunKPMhvG8ThF7EJxrBs=" }
+		@cookies = { "access-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjMwNDg2MjA1MzYsImp0aSI6Imk3c3FlRVNFREpIVVNCWmQ0SEpONDJvMSIsImlzcyI6ImxvY2FsaG9zdDozMDAwIiwiY3NyZl90b2tlbiI6Ik5JQnprYS8zUGxqOHlnMzArdVlueUVCR3VuS1BNaHZHOFRoRjdFSnhkckJzPSJ9.P01mbi5sMnrRr17u79srYfEgkO66eYEK8iFc0n3vIrY",
+					 "refresh-token" => "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjMwODAxNTY4MTYsImp0aSI6Imk3c3FlRVNFREpIVVNCWmQ0SEpONDJvMSIsImlzcyI6ImxvY2FsaG9zdDozMDAwIn0.COOnUxFFyiWUl6DNbUloGNllh8GFeD8OKgNhQr7HCtA" }
+
+		@headers.merge!(headers) if headers
+		@cookies.merge!(cookies) if cookies
+		@body.merge!(body) 		 if body
+	end
+
+###
+# access token:
+# {
+#   "alg": "HS256",
+#   "typ": "JWT"
+# }
+# {
+#   "exp": 3048620536,
+#   "jti" : "i7sqeESEDJHUSBZd4HJN42o1",
+#   "iss": "localhost:3000",
+#   "csrf_token": "NIBzka/3Plj8yg30+uYnyEBGunKPMhvG8ThF7EJxrBs="
+# }
+# HMACSHA256(
+# secret
+# )
+###
+#
+###
+# refresh token:
+#{
+  # "exp": 3080156816,
+  # "jti": "i7sqeESEDJHUSBZd4HJN42o1",
+  # "iss": "localhost:3000"
+#}
+#
+end
--- spec/models/jwt_authenticator_spec.rb
+++ spec/models/jwt_authenticator_spec.rb
@@ -1,13 +1,74 @@
 require 'rails_helper'
+require 'helpers/mock_request.rb'
 
-RSpec.describe JWTAuthenticator2, type: :request do
+RSpec.describe JWTAuthenticator, type: :request do
 
-	before do
-		get v1_users_path, constraints: {subdomain: 'api'}
+	context 'valid request' do
+
+		let(:request) { MockRequest.new(valid = true) }
+		let(:csrf) { "NIBzka/3Plj8yg30+uYnyEBGunKPMhvG8ThF7EJxrBs=" }
+
+		describe 'authenticate' do
+
+		end
+
+		it 'access request should contain X-XSRF-TOKEN and access-token' do
+			expect(request.headers).to include('X-XSRF-TOKEN')
+			expect(request.cookies).to include('access-token')
+			expect(JWTAuthenticator.validate_access_request(request)).to be_truthy
+		end
+
+		it 'refresh request should contain X-XSRF-TOKEN, access-token and refresh-token' do
+			expect(request.headers).to include('X-XSRF-TOKEN')
+			expect(request.cookies).to include('access-token')
+			expect(request.cookies).to include('refresh-token')
+			expect(JWTAuthenticator.validate_refresh_request(request)).to be_truthy
+		end
+
+		it 'encoding with csrf should return a valid access-token' do
+			user = FactoryGirl.create(:user)
+			token = JWTAuthenticator.encode_token(user, Time.now, csrf)
+
+			exception = false
+			begin
+				decoded_token = JWTAuthenticator.decode_token(token)
+			rescue => e
+				exception = e.message
+			end
+
+			expect(decoded_token.first["csrf_token"]).to eq(csrf)
+		end
+
+		it 'encoding without csrf should return a valid refresh-token' do
+			user = FactoryGirl.create(:user)
+			token = JWTAuthenticator.encode_token(user, Time.now)
+
+			exception = false
+			begin
+				decoded_token = JWTAuthenticator.decode_token(token)
+			rescue => e
+				exception = e.message
+			end
+
+			expect(decoded_token).to be_truthy
+			expect(decoded_token.first).to_not include("csrf_token")
+		end
+
+		it 'decoding should be valid' do
+			exception = false
+			begin
+				decoded_token = JWTAuthenticator.decode_token(request.cookies["access-token"])
+			rescue => e
+				exception = e.message
+			end
+
+			expect(exception).to be_falsy
+			expect(decoded_token).to be_truthy
+		end
 	end
 
-	it 'testddd' do
-		expect(JWTAuthenticator2.validate_request(request).maimou).to be_falsy
+	context 'invalid request' do
+		let(:request) { MockRequest.new(valid = false) }
 	end
 
 end
