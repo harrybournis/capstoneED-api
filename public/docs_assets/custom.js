0c50545e89f74909707028c2f43e293b876b2135
harry bournis <harrybournis@gmail.com>
2016-09-10 18:37:30 +0300

    commit before making big changes

---
+++ public/docs_assets/custom.js
@@ -0,0 +1,8 @@
+$(function() {
+	$('#nav').affix({
+	    offset: {
+	      top: $('#nav').offset().top,
+	      bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
+	    }
+	});
+});
