65b01d68bd7891fe25438bd45e0cee16264b1224
harry bournis <harrybournis@gmail.com>
2016-09-18 19:47:47 +0300

    changed projects create to accept attributes for teams

--- app/controllers/v1/teams_controller.rb
+++ app/controllers/v1/teams_controller.rb
@@ -96,10 +96,8 @@ class V1::TeamsController < ApplicationController
 		if student = @team.students.find_by(id: params[:student_id])
 			@team.students.delete(student)
 			render json: '', status: 204
-			binding.pry
 		else
 			render json: format_errors({ base: ["Can't find Student with id #{params[:student_id]} in this Team."] }), status: :unprocessable_entity
-			binding.pry
 		end
 	end
 
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,13 +1,14 @@
 class Team < ApplicationRecord
+  # Attributes: name, enrollment_key, logo, project_id
 
 	# Associations
-  belongs_to  :project
+  belongs_to  :project, inverse_of: :teams
   has_many    :students_teams, class_name: JoinTables::StudentsTeam
   has_many    :students, through: :students_teams, dependent: :delete_all
   has_one     :lecturer,  through: :project
 
   # Validations
-  validates_presence_of 	:name, :enrollment_key, :project_id
+  validates_presence_of 	:name, :enrollment_key, :project
   validates_uniqueness_of :id, :enrollment_key
   validates_uniqueness_of :name, scope: :project_id, case_sensitive: false
 
