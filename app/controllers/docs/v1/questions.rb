04604f84fcdfbe2c88f63070af249dade8517c3d
harry bournis <harrybournis@gmail.com>
2016-09-18 16:10:17 +0300

    updated docs for questions

--- app/controllers/docs/v1/questions.rb
+++ app/controllers/docs/v1/questions.rb
@@ -10,63 +10,38 @@ class Docs::V1::Questions < ApplicationController
 	  api_version 'v1'
 	  meta attributes: { category: 'String', text: 'String', lecturer_id: 'Integer' }
 	  description <<-EOS
-			Lecturers build forms from questions. Questions can either be chosen from a predefined
-			set of questions, or a Lecturer can create their own custom question.
+			Lecturers build forms from questions. If a lecturer has used a Question in a form, it will be
+			available for them to choose in later forms.
 	  EOS
 	end
 
-	api :GET, '/predefined_questions', "Get all the predefined questions"
-  meta :authentication? => true
-  meta :lecturers_only => true
-  error code: 401, desc: 'Authentication failed'
- 	error code: 403, desc: 'Current User is not a Lecturer.'
-  description <<-EOS
-  	Get all predefined questions. Must be a Lecturer.
-  EOS
-  def index_predefined_questions
-  end
-
-  api :GET, '/custom_questions', "Current User's custom questions"
+  api :GET, '/questions', "Current User's questions"
   meta :authentication? => true
   meta :lecturers_only => true
   error code: 401, desc: 'Authentication failed'
  	error code: 403, desc: 'Current User is not a Lecturer.'
 	error code: 403, desc: 'Current User is not associated with this resource'
   description <<-EOS
-  	Get the current user's custom questions
+  	Get the current user's questions
   EOS
-  def index_custom_questions
+  def index_questions
   end
 
-	api :GET, 'predefined_questions/:id', 'Show a predefined question'
+	api :GET, 'custom_questions/:id', 'Show a question'
   meta :authentication? => true
   meta :lecturers_only => true
-  param :id, Integer, 'The id of the Predefined Question to be returned', required: true
+  param :id, Integer, 'The id of the Question to be returned', required: true
   error code: 401, desc: 'Authentication failed'
  	error code: 403, desc: 'Current User is not a Lecturer.'
 	error code: 403, desc: 'This User is not the owner of this resource'
 	description <<-EOS
-		Returns the Predefined Question specified by the id in the params. The User must be logged in, a Lecturer,
-		and the Predefined Question must belong to them.
+		Returns the Question specified by the id in the params. The User must be logged in, a Lecturer,
+		and the Question must belong to them.
 	EOS
-	def show_custom_question
+	def show_question
 	end
 
-	api :GET, 'custom_questions/:id', 'Show a custom question'
-  meta :authentication? => true
-  meta :lecturers_only => true
-  param :id, Integer, 'The id of the Custom Question to be returned', required: true
-  error code: 401, desc: 'Authentication failed'
- 	error code: 403, desc: 'Current User is not a Lecturer.'
-	error code: 403, desc: 'This User is not the owner of this resource'
-	description <<-EOS
-		Returns the Custom Question specified by the id in the params. The User must be logged in, a Lecturer,
-		and the Custom Question must belong to them.
-	EOS
-	def show_custom_question
-	end
-
-	api :POST, '/custom_questions', 'Create a new Custom Question resource'
+	api :POST, '/questions', 'Create a new Question resource'
 	meta :authentication? => true
   meta :lecturers_only => true
 	param :text, 		String, "The description of the question for the students", 		required: true
@@ -75,25 +50,24 @@ class Docs::V1::Questions < ApplicationController
 	error code: 403, desc: 'Current User is not a lecturer'
 	error code: 422, desc: "Text or Category are missing from the params"
 	description <<-EOS
-		Create a new Custom Question.
+		Create a new Question.
 	EOS
-	def create
+	def create_question
 	end
 
-	api :PATCH, '/custom_questions/:id', 'Update Custom Question'
+	api :PATCH, '/questions/:id', 'Update Question'
 	meta :authentication? => true
   meta :lecturers_only => true
-	param :id, String, 'The id of the Custom Question', required: true
+	param :id, String, 'The id of the Question', required: true
 	param :text, 		String, "The description of the question for the students"
 	param :category, 	String, "The type of question. Can be Question or Comment"
 	error code: 401, desc: 'Authentication failed'
 	error code: 403, desc: 'Current User is not a lecturer'
 	error code: 403, desc: 'This User is not the owner of this resource'
-	error code: 422, desc: "University or name are missing from the params"
 	description <<-EOS
-		Update a Custom Question resource.
+		Update a Question resource.
 	EOS
-	def update
+	def update_question
 	end
 
 	api :DELETE, '/custom_questions/:id', 'Delete Custom Question'
@@ -103,10 +77,9 @@ class Docs::V1::Questions < ApplicationController
 	error code: 401, desc: 'Authentication failed'
 	error code: 403, desc: 'Current User is not a lecturer'
 	error code: 403, desc: 'This User is not the owner of this resource'
-	error code: 422, desc: "University or name are missing from the params"
 	description <<-EOS
-		Delete Custom Question resource
+		Delete Question resource
 	EOS
-	def destroy
+	def destroy_question
 	end
 end
