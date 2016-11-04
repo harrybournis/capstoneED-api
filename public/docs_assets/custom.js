7e3dbb4e4fe1c7ea96ad543674f248f23ce32c84
harry bournis <harrybournis@gmail.com>
2016-11-04 12:13:17 +0200

    fixed docs responsiveness on mobile

--- public/docs_assets/custom.js
+++ public/docs_assets/custom.js
@@ -1,8 +1,10 @@
 $(function() {
-	$('#nav').affix({
-	    offset: {
-	      top: $('#nav').offset().top//,
-	      //bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
-	    }
-	});
+	if ($(window).width() >= 768) {
+		$('#nav').affix({
+		    offset: {
+		      top: $('#nav').offset().top//,
+		      //bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
+		    }
+		});
+	}
 });
