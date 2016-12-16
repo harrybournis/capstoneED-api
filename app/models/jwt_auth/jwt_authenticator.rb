d87efbaa48ac78d372ff597c4f85a0d081181a9e
harry bournis <harrybournis@gmail.com>
2016-12-16 16:41:11 +0200

    updated gems

--- app/models/jwt_auth/jwt_authenticator.rb
+++ app/models/jwt_auth/jwt_authenticator.rb
@@ -1,17 +1,17 @@
 module JWTAuth::JWTAuthenticator
 
 	# Class Variables
-	@@secret			= 'secret' 			## TO be replaced with the application's secret key
-	@@algorithm 	= 'HS256'				# available algorithms: https://github.com/jwt/ruby-jwt
-	@@exp					= 10.minutes		# expiration time for access-token
-	@@refresh_exp	= 1.week				# expiration time for refresh-token
-	@@leeway			= 0							# grace period after a token has expired.
-	@@domain  		= api_host_url	# to be added to the cookies. left blank for developement in order to work with browsers. Change variable in helpers/url_helper.rb
-	@@issuer			= @@domain			# typically the website url. added to JWT tokens.
+	@secret			= 'secret' 			## TO be replaced with the application's secret key
+	@algorithm 	= 'HS256'				# available algorithms: https://github.com/jwt/ruby-jwt
+	@exp					= 10.minutes		# expiration time for access-token
+	@refresh_exp	= 1.week				# expiration time for refresh-token
+	@leeway			= 0							# grace period after a token has expired.
+	@domain  		= api_host_url	# to be added to the cookies. left blank for developement in order to work with browsers. Change variable in helpers/url_helper.rb
+	@issuer			= @domain			# typically the website url. added to JWT tokens.
 
-	@@cookies_secure 		= false # transmit cookies only on https
-	@@cookies_httponly 	= true 	# javascript can't read cookies
-	@@cookies_samesite	= false	# send cookies only if url in address bar matches the current site
+	@cookies_secure 		= false # transmit cookies only on https
+	@cookies_httponly 	= true 	# javascript can't read cookies
+	@cookies_samesite	= false	# send cookies only if url in address bar matches the current site
 
 	###
 	# Called before an authenticated endpoint in before_action in application_controller.
@@ -53,7 +53,7 @@ module JWTAuth::JWTAuthenticator
 		time_now = Time.now
 
 		if create_new_tokens(user, response, cookies, new_device, time_now, remember_me)
-			new_token = ActiveToken.new(exp: time_now + @@refresh_exp, device: new_device, user: user)
+			new_token = ActiveToken.new(exp: time_now + @refresh_exp, device: new_device, user: user)
 
 			if new_token.valid?
 				user.active_tokens << new_token
@@ -88,7 +88,7 @@ module JWTAuth::JWTAuthenticator
 			cookies.delete('refresh-token')
 
 			if create_new_tokens(valid_token.user, response, cookies, device, time_now, remember_me)
-				return true if valid_token.update(exp: time_now + @@refresh_exp)
+				return true if valid_token.update(exp: time_now + @refresh_exp)
 			end
 		end
 		false
@@ -100,8 +100,8 @@ module JWTAuth::JWTAuthenticator
 	#### PRIVATE METHODS
 	def self.create_new_tokens (user, response, cookies, device, time_now, remember_me)
 		csrf_token			  = SecureRandom.urlsafe_base64(32)
-		exp_time 			  	= time_now + @@exp
-		refresh_exp_time	= time_now + @@refresh_exp
+		exp_time 			  	= time_now + @exp
+		refresh_exp_time	= time_now + @refresh_exp
 
 		access_token 		  = encode_token(user, time_now, csrf_token, device)
 		refresh_token		  = encode_token(user, time_now, nil, device, remember_me)
@@ -112,11 +112,11 @@ module JWTAuth::JWTAuthenticator
 
 		if Rails.env.production?
 			if remember_me
-				cookies['access-token'] = { value: access_token, domain: @@domain, expires: exp_time, httponly: @@cookies_httponly, same_site: @@cookies_samesite }
-				cookies['refresh-token'] = { value: refresh_token, domain: @@domain, expires: refresh_exp_time, path: '/v1/refresh', httponly: @@cookies_httponly, same_site: @@cookies_samesite }
+				cookies['access-token'] = { value: access_token, domain: @domain, expires: exp_time, secure: @cookies_secure, httponly: @cookies_httponly, same_site: @cookies_samesite }
+				cookies['refresh-token'] = { value: refresh_token, domain: @domain, expires: refresh_exp_time, path: '/v1/refresh', secure: @cookies_secure, httponly: @cookies_httponly, same_site: @cookies_samesite }
 			else
-				cookies['access-token'] = { value: access_token, domain: @@domain, secure: @@cookies_secure, httponly: @@cookies_httponly, same_site: @@cookies_samesite }
-				cookies['refresh-token'] = { value: refresh_token, domain: @@domain, path: '/v1/refresh', secure: @@cookies_secure, httponly: @@cookies_httponly, same_site: @@cookies_samesite }
+				cookies['access-token'] = { value: access_token, domain: @domain, secure: @cookies_secure, httponly: @cookies_httponly, same_site: @cookies_samesite }
+				cookies['refresh-token'] = { value: refresh_token, domain: @domain, path: '/v1/refresh', secure: @cookies_secure, httponly: @cookies_httponly, same_site: @cookies_samesite }
 			end
 
 		elsif Rails.env.test? # removed path from refresh token to work with rspec
@@ -144,30 +144,30 @@ module JWTAuth::JWTAuthenticator
 
 	def self.encode_token (user, time_now, csrf_token = nil, device_id = nil, remember_me = false)
 		if csrf_token
-			exp_time = time_now + @@exp
-			access_token_payload = { exp: exp_time.to_i, id: user.id, type: user.type, iss: @@issuer, device: device_id, csrf_token: csrf_token }
+			exp_time = time_now + @exp
+			access_token_payload = { exp: exp_time.to_i, id: user.id, type: user.type, iss: @issuer, device: device_id, csrf_token: csrf_token }
 
-			JWT.encode(access_token_payload, @@secret, @@algorithm)
+			JWT.encode(access_token_payload, @secret, @algorithm)
 
 		else
-			refresh_exp_time 			= time_now + @@refresh_exp
-			refresh_token_payload = { exp: refresh_exp_time.to_i, iss: @@issuer, device: device_id, remember_me: remember_me }
+			refresh_exp_time 			= time_now + @refresh_exp
+			refresh_token_payload = { exp: refresh_exp_time.to_i, iss: @issuer, device: device_id, remember_me: remember_me }
 
-			JWT.encode(refresh_token_payload, @@secret, @@algorithm)
+			JWT.encode(refresh_token_payload, @secret, @algorithm)
 		end
 	end
 
 
 	def self.decode_token (token)
 		if Rails.env.production?
-			JWT.decode(token, @@secret, true, { algorithm: @@algorithm,
-									leeway: @@leeway.to_i,
-									iss: @@issuer,
+			JWT.decode(token, @secret, true, { algorithm: @algorithm,
+									leeway: @leeway.to_i,
+									iss: @issuer,
 									verify_iss: true
 															})
 		else
-			JWT.decode(token, @@secret, true, { algorithm: @@algorithm,
-												leeway: @@leeway.to_i})
+			JWT.decode(token, @secret, true, { algorithm: @algorithm,
+												leeway: @leeway.to_i})
 		end
 	end
 
@@ -192,6 +192,6 @@ module JWTAuth::JWTAuthenticator
 	end
 
 
-	def self.refresh_exp ; @@refresh_exp end
-	def self.domain		 ; @@domain end
+	def self.refresh_exp ; @refresh_exp end
+	def self.domain		 ; @domain end
 end
