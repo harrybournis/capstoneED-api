6ab4a58bbc6802a2751de4c219de1472329b8385
harry bournis <harrybournis@gmail.com>
2017-01-10 13:51:01 +0200

    added unit_id in units serializer

--- app/serializers/assignment_serializer.rb
+++ app/serializers/assignment_serializer.rb
@@ -1,5 +1,5 @@
 class AssignmentSerializer < Base::BaseSerializer
-  attributes :start_date, :end_date, :name
+  attributes :start_date, :end_date, :name, :unit_id
 
   attribute :href do
   	"/assignments/#{object.id}"
