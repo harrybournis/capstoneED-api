9c6a046ebb4316f9c8d3dc2f81e55409a5d5a897
Ioannis Boutsikas <mgioanni@outlook.com>
2017-03-08 17:35:45 +0200

    Added different student and lecturer origins in cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,23 @@
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+# Handle Cross-Origin Resource Sharing (CORS) in order to
+# accept cross-origin AJAX requests.
 
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'capstoneed-dev.org:8080'
+    origins 'student.capstoneed-dev.org:8080'
+
+    resource '*',
+      headers: :any,
+      expose: 'XSRF-TOKEN',
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'lecturer.capstoneed-dev.org:8085'
 
     resource '*',
       headers: :any,
