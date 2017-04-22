d38f4019da4a17f211cac4b079ac1ddb48b2ddc4
harry bournis <harrybournis@gmail.com>
2017-04-22 03:30:02 +0300

    Added Marking::Results class

--- app/services/marking/results.rb
+++ app/services/marking/results.rb
@@ -1,5 +1,68 @@
 module Marking
   class Results
+    def initialize
+      @scores = {}
+      @marks = {}
+    end
 
+    # Returns true if there are marks.
+    #
+    # @return [Boolean] True if marks is not empty.
+    #
+    def marked?
+      @marks.any?
+    end
+
+    # Returns the score for the provided
+    # student_id.
+    #
+    # @param student_id [Integer] The id of the student.
+    #
+    # @return [Number] The score.
+    #
+    def score(student_id)
+      @scores[student_id]
+    end
+
+    # Returns the mark for the provided
+    # student_id.
+    #
+    # @param student_id [Integer] The id of the student.
+    #
+    # @return [Number] The mark.
+    #
+    def mark(student_id)
+      @marks[student_id]
+    end
+
+    # Add a new score for a student_id. Returns nil if
+    # student_id already has a score.
+    #
+    # @param student_id [Integer] The id of the Student
+    #   receiving the score.
+    # @param score [Integer] The score.
+    #
+    # @return [Boolean,nil] Returns true if successully added, nil
+    #   if the student already has a score.
+    #
+    def add_score(student_id, score)
+      return nil if @scores[student_id]
+      @scores[student_id] = score
+    end
+
+    # Add a new mark for a student_id. Returns nil if
+    # student_id already has a mark.
+    #
+    # @param student_id [Integer] The id of the Student
+    #   receiving the mark.
+    # @param score [Integer] The mark.
+    #
+    # @return [Boolean,nil] Returns true if successully added, nil
+    #   if the student already has a mark.
+    #
+    def add_mark(student_id, mark)
+      return nil if @marks[student_id]
+      @marks[student_id] = mark
+    end
   end
 end
--- spec/main_tests/services/marking_algorithms/results_spec.rb
+++ spec/main_tests/services/marking_algorithms/results_spec.rb
@@ -1,14 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe MarkingAlgorithms::Results, type: :model do
-  context 'contains scores' do
-    it 'marked? returns false'
-    @results.score(student_id)
-    @results.mark(student_id)
+RSpec.describe Marking::Results, type: :model do
+  before :each do
+    @student_id1 = 1
+    @student_id2 = 2
+    @score1 = 0.83
+    @score2 = 1.13
+    @mark1 = 60
+    @mark2 = 58
+    @results = Marking::Results.new
   end
 
-  context 'contains marks' do
-    it 'marked? returns false'
+  it '#add_score adds the score for the student_id' do
+    expect(@results.score(@student_id1)).to be_falsy
+    expect(@results.add_score(@student_id1, @score1)).to be_truthy
+    expect(@results.score(@student_id1)).to eq @score1
+  end
+
+  it '#add_score returns nil if student already has a score' do
+    expect(@results.add_score(@student_id1, @score1)).to be_truthy
+    expect(@results.add_score(@student_id1, @score1)).to be_falsy
+  end
+
+  it '#add_mark adds the mark for the student_id' do
+    expect(@results.mark(@student_id1)).to be_falsy
+    expect(@results.add_mark(@student_id1, @mark1)).to be_truthy
+    expect(@results.mark(@student_id1)).to eq @mark1
+  end
+
+  it '#add_mark returns nil if student already has a mark' do
+    expect(@results.add_mark(@student_id1, @mark1)).to be_truthy
+    expect(@results.add_mark(@student_id1, @mark1)).to be_falsy
+  end
+
+  it '#marked? returns false if no marks are set' do
+    expect(@results.marked?).to be_falsy
+    @results.add_mark(@student_id1, @mark1)
+    expect(@results.marked?).to be_truthy
+  end
+
+  it '#score(student_id) returns the score for the student_id' do
+    @results.add_score(@student_id1, @score1)
+    expect(@results.score(@student_id1)).to eq @score1
+  end
+
+  it '#score(student_id) returns nil if student_id does not exist' do
+    expect(@results.score(@student_id1)).to be_falsy
+  end
+
+  it '#mark(student_id) returns nil if student_id does not exist' do
+    expect(@results.mark(@student_id1)).to be_falsy
+  end
+  it '#mark(student_id) returns the mark for the student_id' do
+    @results.add_mark(@student_id1, @mark1)
+    expect(@results.mark(@student_id1)).to eq @mark1
   end
 end
 
