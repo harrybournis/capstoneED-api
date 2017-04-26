d6deec814d07ded48cca357f14fb9ba89563ba80
harry bournis <harrybournis@gmail.com>
2017-04-26 13:38:14 +0300

    Changed Question question_type_id attribute to type_id to match with the questions in the pa_form

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,4 +1,8 @@
 class QuestionSerializer < Base::BaseSerializer
-	attributes :question_type_id, :text
+	attributes :type_id, :text
   has_one :question_type
+
+  def type_id
+    object.question_type_id
+  end
 end
