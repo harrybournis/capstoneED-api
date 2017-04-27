f7cb0e6d290af1680878b2c760565f7f34f99aa6
harry bournis <harrybournis@gmail.com>
2017-04-27 18:55:07 +0300

    Added question_type inside FormTemplateSerializer

--- app/serializers/form_template_serializer.rb
+++ app/serializers/form_template_serializer.rb
@@ -1,3 +1,20 @@
 class FormTemplateSerializer < Base::BaseSerializer
-  attributes :name, :questions, :lecturer_id
+  attributes :name, :lecturer_id
+  attribute :questions
+
+  def questions
+    question_types = QuestionType.all
+
+    object.questions.tap do |questions|
+      questions.each do |q|
+        question_types.each do |t|
+          if t.id == q['type_id']
+            q.merge!(ActiveModelSerializers::SerializableResource.new(t, {serializer: QuestionTypeSerializer})
+                                                                             .as_json)
+          end
+        end
+      end
+      questions.to_json
+    end
+  end
 end
