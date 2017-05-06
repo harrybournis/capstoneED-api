57ed880e824b0663322eb1d02aba05b7dad0e02b
harry bournis <harrybournis@gmail.com>
2017-05-07 02:35:27 +0300

    Updated routes to handle bad requests to /stats

--- spec/main_tests/routing/routing_graphs_spec.rb
+++ spec/main_tests/routing/routing_graphs_spec.rb
@@ -19,6 +19,11 @@ RSpec.describe 'Stats Routing', type: :routing do
       controller: 'v1/logs/stats', action: 'hours_worked_assignment', graph: "hours_worked", assignment_id: "3")
   end
 
+  it 'GET /stats?graph=hours_worked without assignment_id or project_id routes to project to show error' do
+    expect(get: "#{url}/v1/stats?graph=hours_worked").to route_to(
+      controller: 'v1/logs/stats', action: 'hours_worked_project', graph: "hours_worked")
+  end
+
   it 'GET /stats?graph=logs_heatmap&project_id routes to logs_heatmap' do
     expect(get: "#{url}/v1/stats?graph=logs_heatmap&project_id=5").to route_to(controller: 'v1/logs/stats', action: 'logs_heatmap', graph: 'logs_heatmap', project_id: "5")
   end
