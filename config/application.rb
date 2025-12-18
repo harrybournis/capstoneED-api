81c719aac398ec870d7e0855ebbc6c6eb907cf12
Harry Bournis <harrybournis@gmail.com>
2025-12-18 20:47:46 +0200

    Remove unneeded application.rb requires and old configs

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,25 @@
 require_relative "boot"
 
-require "rails/all"
+require "rails"
+
+# action_view/railtie
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_mailer/railtie
+  active_job/railtie
+).each do |railtie|
+  begin # rubocop:disable Style/RedundantBegin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CapstoneedApiNew
+module CapstoneedApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
